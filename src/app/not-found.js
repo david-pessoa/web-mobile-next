9a8e57d0e22e83284e0b50e6958e5cea43cb7532
david-pessoa <pessoa.dvlbp@gmail.com>
2025-10-27 16:40:24 -0300

    add página de erro 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 import style from '../styles/notFound.module.css';
 
+export const metadata = {
+  title: 'Erro 404',
+};
+
 export default function NotFound() {
   return (
     <div className={style.container}>
       <div className={`${style.box} dark-background`}>
-        <title> Erro 404</title>
         <h1 className="title medium">Erro 404</h1>
         <h3 className="not_found_message">Página não encontrada</h3>
         <Link className="not_found" href="/">
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,25 @@
-import Header from "@/components/header/Header"
-import HomeImg from "@/components/homeImg/HomeImg";
-import Especialidades from "@/components/especialidades/Especialidades";
-import Sobre from "@/components/sobre/Sobre";
-import StatusBar from "@/components/statusBar/StatusBar";
-import Depoimentos from "@/components/depoimentos/Depoimentos";
-import Footer from "@/components/footer/Footer";
+import Header from '@/components/header/Header';
+import HomeImg from '@/components/homeImg/HomeImg';
+import Especialidades from '@/components/especialidades/Especialidades';
+import Sobre from '@/components/sobre/Sobre';
+import StatusBar from '@/components/statusBar/StatusBar';
+import Depoimentos from '@/components/depoimentos/Depoimentos';
+import Footer from '@/components/footer/Footer';
+
+export const metadata = {
+  title: 'Consultório Odontológico',
+};
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <HomeImg/>
-      <Especialidades/>
-      <Sobre/>
-      <StatusBar/>
-      <Depoimentos/>
-      <Footer/>
+      <Header />
+      <HomeImg />
+      <Especialidades />
+      <Sobre />
+      <StatusBar />
+      <Depoimentos />
+      <Footer />
     </div>
   );
 }
