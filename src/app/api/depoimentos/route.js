6f6462792a5cb04abb72ae3ff05235f9d9a5eee0
david-pessoa <pessoa.dvlbp@gmail.com>
2026-01-06 22:25:33 -0300

    cria contact form e ajusta padding do menu mobile

--- src/app/api/depoimentos/route.js
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-static";
-import { NextResponse } from "next/server";
-import data from "@/app/api/dados.json";
-
-export async function GET() {
-  return NextResponse.json(data.depoimentos)
-}
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,93 +2,12 @@ import styles from '../../styles/footer.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import ContactForm from './ContactForm';
 export default function Footer() {
   return (
     <footer id="contato" className={styles.contato}>
       <section>
-        <form id="form" data-parsley-validate>
-          <h4>Formulário de Contato</h4>
-
-          <fieldset>
-            <label htmlFor="nome">Nome</label>
-            <input
-              type="text"
-              id="nome"
-              name="nome"
-              required
-              data-parsley-required-message="Por favor, preencha seu nome"
-            />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              required
-              data-parsley-required-message="Por favor, preencha seu email"
-              data-parsley-type-message="Por favor, insira um email válido"
-            />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="assunto">Assunto</label>
-            <input
-              type="text"
-              id="assunto"
-              name="assunto"
-              required
-              data-parsley-required-message="Por favor, preencha o assunto"
-            />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="especialidade">Selecione uma especialidade</label>
-            <select
-              id="especialidade"
-              name="especialidade"
-              required
-              data-parsley-required-message="Por favor, selecione uma especialidade"
-            >
-              <option value="">Selecione</option>
-              <option>Estética</option>
-              <option>Dentística</option>
-              <option>Periodontia</option>
-              <option>Endodontia</option>
-              <option>Ortodontia</option>
-              <option>Implantodontia</option>
-            </select>
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="modalidade">Modalidade</label>
-            <select
-              id="modalidade"
-              name="modalidade"
-              required
-              data-parsley-required-message="Por favor, selecione uma modalidade"
-            >
-              <option value="">Selecione</option>
-              <option>Presencial</option>
-              <option>Teleconsulta</option>
-            </select>
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="mensagem">Mensagem</label>
-            <textarea
-              id="mensagem"
-              name="mensagem"
-              required
-              data-parsley-required-message="Por favor, selecione uma mensagem"
-            ></textarea>
-          </fieldset>
-
-          <button type="submit" className="medium">
-            Enviar
-          </button>
-        </form>
+        <ContactForm />
 
         <iframe
           className="mapa"
